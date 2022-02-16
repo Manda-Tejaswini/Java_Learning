6cd8e05dcd583b42bb2247ec3609cd6d2b6070c1
Manda-Tejaswini <viswatejaswini0114@gmail.com>
2022-02-16 12:45:42 +0000

    first commit

---
+++ src/Typecasting.java
@@ -0,0 +1,37 @@
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//implicit conversion
+				System.out.println("Implicit Type Casting");
+				char v='T';
+				System.out.println("Value of v: "+v);
+				
+				int w=v;
+				System.out.println("Value of w: "+w);
+				
+				float x=v;
+				System.out.println("Value of x: "+x);
+				
+				long y=v;
+				System.out.println("Value of y: "+y);
+				
+				double z=v;
+				System.out.println("Value of z: "+z);
+				
+						
+				System.out.println("\n");
+				
+				System.out.println("Explicit Type Casting");
+				//explicit conversion
+				
+				double a=97.8;
+				int b=(int)a;
+				System.out.println("Value of x: "+a);
+				System.out.println("Value of y: "+b);
+
+
+	}
+
+}
