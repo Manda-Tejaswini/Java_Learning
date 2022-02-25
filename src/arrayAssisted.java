523e73874bc321b8e7c74b9b50edef247b8a587b
Manda-Tejaswini <viswatejaswini0114@gmail.com>
2022-02-25 20:52:42 +0530

    created arrayAssisted innerClassAssisted1 innerClassAssisted2 innerClassAssisted3 regularExpnAssisted stringDemo

---
+++ src/arrayAssisted.java
@@ -0,0 +1,19 @@
+public class arrayAssisted {
+
+public static void main(String[] args) {
+
+//single-dimensional array
+int a[]= {50,60,70,20,30};
+for(int i=0;i<5;i++) {
+System.out.println("Elements of array a: "+a[i]);
+}
+
+
+//multidimensional array
+int[][] b = {
+            {2, 4, 6, 8}, 
+            {3, 6, 9} };
+      
+      System.out.println("\nLength of row 1: " + b[0].length);
+      }
+}
