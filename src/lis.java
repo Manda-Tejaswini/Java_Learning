d10a6167dc18a850c455ae2d43628e444e33c01d
Manda-Tejaswini <viswatejaswini0114@gmail.com>
2022-03-01 14:15:19 +0530

    created lis

--- src/lis.java
+++ src/lis.java
@@ -1,10 +1,5 @@
 public class lis {
-
-
-
     static int max_ref; // stores the LIS
-  
-   
     static int _lis(int arr[], int n)
     {
         // base case
@@ -15,13 +10,13 @@ public class lis {
         int res, max_ending_here = 1;
   
        
-        for (int i = 1; i < n; i++) {
+        for (int i =
+  
+        // Compare max_en 1; i < n; i++) {
             res = _lis(arr, i);
             if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
                 max_ending_here = res + 1;
-        }
-  
-        // Compare max_ending_here with the overall max. And
+        }ding_here with the overall max. And
         // update the overall max if needed
         if (max_ref < max_ending_here)
             max_ref = max_ending_here;
