99dd87b4475702607884ff6e0086b8a0ca746453
Manda-Tejaswini <viswatejaswini0114@gmail.com>
2022-03-01 12:27:21 +0530

    created FinallyBlockDemo Main MyClass MyClasss MyRunnableThread ReadFileIntoList Test TextFileModificationProgram ThrowDemo ThrowsDemo bugfix email filehanding lis sample

---
+++ src/sample.java
@@ -0,0 +1,12 @@
+
+public class sample {
+public static void main(String[] args) {
+	int myvalue=10000;
+	int myminIntvalue = Integer.MIN_VALUE;
+	int mymaxIntvalue = Integer.MAX_VALUE;
+	System.out.println("Integer minIntvalue = " + myminIntvalue );
+	System.out.println("Integer maxIntvalue = " + mymaxIntvalue );
+	
+}
+
+}
